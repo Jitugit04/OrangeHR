2d0e433fa083159324ef256ad455ddf13706c44a
Jitendra Agrawal <jitendra.x.agrawal@hotmail.com>
2023-10-13 16:08:09 +0530

    ThreadTime Changes on TestMyinfo.java-@Jitendra

--- com.orangehrm/src/test/java/Testcases/TestMyinfo.java
+++ com.orangehrm/src/test/java/Testcases/TestMyinfo.java
@@ -62,7 +62,7 @@ public class TestMyinfo extends Base {
 			this.myInfoPage.ClickMyinfomenu();
 			// String URL=Assert.assertEquals(driver.getCurrentUrl(),
 			// "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
-			Thread.sleep(3001);
+			Thread.sleep(3002);
 			// driver.navigate().to(driver.getCurrentUrl());
 			// SystemThen.out.println("Page load started at : " + startTime.toString());
 
